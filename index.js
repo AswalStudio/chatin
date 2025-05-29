06e1e2298094a1039188a9cd1f6b000d1fffaed6
AswalStudio <106762467+AswalStudio@users.noreply.github.com>
2025-05-30 02:05:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
