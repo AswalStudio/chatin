b3acafa13ae81b0e5aa7a4b22425dfe47922fbc8
AswalStudio <106762467+AswalStudio@users.noreply.github.com>
2025-05-30 02:28:59 +0530

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  }
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const ROOM_PASSWORDS = {
+  SSSJIS: "#7430$",
+  WAGON: "PAZz0%",
+  Y2M$: "7R0Mnk(i)",
+  CHUPk0: "Az1Bu42&"
+};
+
+io.use((socket, next) => {
+  const { username, room, password } = socket.handshake.auth;
+
+  if (!ROOM_PASSWORDS[room]) return next(new Error("Room does not exist"));
+  if (ROOM_PASSWORDS[room] !== password) return next(new Error("Incorrect password"));
+
+  socket.username = username;
+  socket.room = room;
+  next();
 });
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+  const { username, room } = socket;
+
+  socket.join(room);
+  socket.to(room).emit("message", `${username} joined ${room}`);
 
   socket.on("message", (msg) => {
-    socket.broadcast.emit("message", msg);
+    io.to(room).emit("message", `${username}: ${msg}`);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    socket.to(room).emit("message", `${username} left`);
   });
 });
 
